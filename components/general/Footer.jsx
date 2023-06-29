9065a2a9c4dff65cbd57755f37b17d2bf90adac1
Stevan <stevanvlajic5@gmail.com>
2023-06-29 11:35:15 +0200

    feat: transition for footer added

--- components/general/Footer.jsx
+++ components/general/Footer.jsx
@@ -11,10 +11,10 @@ export default function Footer() {
         <h3 className="font-black text-2xl border-b-4 border-sky-300 mb-2">Contact</h3>
         <div className="font-bold">
           <div className="font-medium">
-            <li className="p-1 rounded-md transition duration-150 ease-in hover:bg-sky-300 hover:text-white"><Link href="">PENIS</Link></li>
-            <li className="p-1 rounded-md transition duration-150 ease-in hover:bg-sky-300 hover:text-white"><Link href="">PENIS</Link></li>
-            <li className="p-1 rounded-md transition duration-150 ease-in hover:bg-sky-300 hover:text-white"><Link href="">PENIS</Link></li>
-            <li className="p-1 rounded-md transition duration-150 ease-in hover:bg-sky-300 hover:text-white"><Link href="">PENIS</Link></li>
+            <li className="p-1 cursor-pointer rounded-md transition duration-150 ease-in-out hover:bg-sky-300 hover:text-white"><Link href="" target="_blank">PENIS</Link></li>
+            <li className="p-1 cursor-pointer rounded-md transition duration-150 ease-in-out hover:bg-sky-300 hover:text-white"><Link href="" target="_blank">PENIS</Link></li>
+            <li className="p-1 cursor-pointer rounded-md transition duration-150 ease-in-out hover:bg-sky-300 hover:text-white"><Link href="" target="_blank">PENIS</Link></li>
+            <li className="p-1 cursor-pointer rounded-md transition duration-150 ease-in-out hover:bg-sky-300 hover:text-white"><Link href="" target="_blank">PENIS</Link></li>
           </div>
         </div>
       </div>
@@ -22,20 +22,20 @@ export default function Footer() {
       <div className="flex flex-col rounded-lg bg-white py-4 px-10 w-60 text-left">
         <h3 className="font-black text-2xl border-b-4 border-[#FFA7CF] mb-2">Top projects</h3>
         <div className="font-medium">
-          <li className="p-1 rounded-md transition duration-150 ease-in hover:bg-[#FFA7CF] hover:text-white"><Link href="https://github.com/Stevan06v/sensorify">Sensorify</Link> </li>
-          <li className="p-1 rounded-md transition duration-150 ease-in hover:bg-[#FFA7CF] hover:text-white"><Link href="https://github.com/Stevan06v/nextjs-hidden-service">Hidden service</Link></li>
-          <li className="p-1 rounded-md transition duration-150 ease-in hover:bg-[#FFA7CF] hover:text-white"><Link href="https://github.com/Stevan06v/plantify">Plantify</Link></li>
-          <li className="p-1 rounded-md transition duration-150 ease-in hover:bg-[#FFA7CF] hover:text-white"><Link href="https://github.com/Stevan06v/coffee-machine">Coffee machine</Link></li>
+          <li className="p-1 cursor-pointer rounded-md transition duration-150 ease-in-out hover:bg-[#FFA7CF] hover:text-white"><Link href="https://github.com/Stevan06v/sensorify" target="_blank">Sensorify</Link> </li>
+          <li className="p-1 cursor-pointer rounded-md transition duration-150 ease-in-out hover:bg-[#FFA7CF] hover:text-white"><Link href="https://github.com/Stevan06v/nextjs-hidden-service" target="_blank">Hidden service</Link></li>
+          <li className="p-1 cursor-pointer rounded-md transition duration-150 ease-in-out hover:bg-[#FFA7CF] hover:text-white"><Link href="https://github.com/Stevan06v/plantify" target="_blank">Plantify</Link></li>
+          <li className="p-1 cursor-pointer rounded-md transition duration-150 ease-in-out hover:bg-[#FFA7CF] hover:text-white"><Link href="https://github.com/Stevan06v/coffee-machine" target="_blank">Coffee machine</Link></li>
         </div>
       </div>
 
       <div className="flex flex-col rounded-lg bg-white py-4 px-10 w-60 text-left">
         <h3 className="font-black text-2xl border-b-4 border-[#9AE6E1] mb-2">Legal</h3>
         <div className="font-medium">
-          <li className="p-1 rounded-md transition duration-150 ease-in hover:bg-[#9AE6E1] hover:text-white"><Link href="">Terms</Link></li>
-          <li className="p-1 rounded-md transition duration-150 ease-in hover:bg-[#9AE6E1] hover:text-white"><Link href="">Privacy policy</Link></li>
-          <li className="p-1 rounded-md transition duration-150 ease-in hover:bg-[#9AE6E1] hover:text-white"><Link href="">Cookies</Link></li>
-          <li className="p-1 rounded-md transition duration-150 ease-in hover:bg-[#9AE6E1] hover:text-white"><Link href="">Imprint</Link></li>
+          <li className="p-1 cursor-pointer rounded-md transition duration-150 ease-in-out hover:bg-[#9AE6E1] hover:text-white"><Link href="" target="_blank">Terms</Link></li>
+          <li className="p-1 cursor-pointer rounded-md transition duration-150 ease-in-out hover:bg-[#9AE6E1] hover:text-white"><Link href="" target="_blank">Privacy policy</Link></li>
+          <li className="p-1 cursor-pointer rounded-md transition duration-150 ease-in-out hover:bg-[#9AE6E1] hover:text-white"><Link href="" target="_blank">Cookies</Link></li>
+          <li className="p-1 cursor-pointer rounded-md transition duration-150 ease-in-out hover:bg-[#9AE6E1] hover:text-white"><Link href="" target="_blank">Imprint</Link></li>
         </div>
       </div>
 
